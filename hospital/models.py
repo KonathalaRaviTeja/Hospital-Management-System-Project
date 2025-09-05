57e67bf57bdcb790c657f4fe8dc7f4ea369b046f
KonathalaRaviTeja <22ucs123nita@gmail.com>
2025-09-05 14:11:20 +0530

    Update models.py

--- hospital/models.py
+++ hospital/models.py
@@ -76,3 +76,12 @@ class PatientDischargeDetails(models.Model):
     OtherCharge=models.PositiveIntegerField(null=False)
     total=models.PositiveIntegerField(null=False)
 
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
+
